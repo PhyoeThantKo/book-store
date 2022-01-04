06ce64105677c10aee5c7bc50f9a94582a379b2e
Phyoe Thant Ko <84618795+PhyoeThantKo@users.noreply.github.com>
2022-01-04 07:46:31 +0630

    add_admin_styles

--- admin/cat-new.php
+++ admin/cat-new.php
@@ -6,6 +6,8 @@
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <title>new category</title>
 
+     <link rel="stylesheet" href="css/style.css">
+
      <style>
           form label{
                display: block;
