280390e8966ae656f295b31651476471acafd875
Phyoe Thant Ko <84618795+PhyoeThantKo@users.noreply.github.com>
2022-01-07 08:28:29 +0630

    show_chosen_cat

--- index.php
+++ index.php
@@ -8,4 +8,13 @@
                $cart += $qty;
           }
      }
+
+     if(isset($_GET['cat'])){
+          $cat_id = $_GET['cat'];
+          $books = mysqli_query($conn, "SELECT * FROM books WHERE category_id = $cat_id");
+     } else {
+          $books = mysqli_query($conn, "SELECT * FROM books");
+     }
+
+     $cats = mysqli_query($conn, "SELECT * FROM categories");
 ?>
